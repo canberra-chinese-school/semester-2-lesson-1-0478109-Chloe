2acbcc6c25f3f09644ba97d1a4ba95ef18e57a5a
stevengao-ytc <stevengao213@gmail.com>
2026-07-28 14:01:00 +1000

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,22 +1,41 @@
-# In class, we have made a simple program that asks the user a random addition maths question.
-# The program generates a random number between 1 to 10
-# and then generates a second number between 1 and 10
-# it asks the user what's the sum of the 2 numbers, and prints "Correct!" if the user gets it right
-
-# TODO: Your task is to improvise this program so that
-# it keeps asking the user until the user gets it right
-# make the whole process into a function, and call this function 5 times (to ask 5 different random addition maths questions)
-
-# Current program:
-import random
-
-number1 = random.randint(1, 10)
-number2 = random.randint(1, 10)
-number_sum = number1 + number2
-user_input = input("What's " + str(number1) + "+" + str(number2) + "?")
+# Edit this file to make a calculator program that:
+# asks the user for the first number
+# then asks the user for an operation choice (plus, minus, multiply, or divide)
+# asks the user for the second number
+# calculates and prints the result
 
 
-if (int(user_input) == number_sum):
-    print("Correct!")
-    
-    
+number1 = int(input("Enter number1: "))
+operation_choice = input("Enter your choice of operation (plus, minus, multiply, divide):")
+number2 = int(input("Enter number2: "))
+
+def plus():
+    result = number1 + number2
+    return result
+
+def minus():
+    result = number1 - number2
+    return result
+
+def multiply():
+    result = number1 * number2
+    return result
+
+def divide():
+    result = number1 / number2
+    return result
+
+
+if operation_choice == "plus":
+    print(plus())
+elif operation_choice == "minus":
+    print(minus())
+elif operation_choice == "multiply":
+    print(multiply())
+elif operation_choice == "divide":
+    print(divide())
+else:
+    print("Please enter a valid operation choice")
+
+
+
